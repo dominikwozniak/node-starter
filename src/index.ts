33a537ff0a7d7884efaf58fbf5fb5612b02ff8f9
dwozniak-guidance <dominik.wozniak@guidance.com>
2022-02-02 22:04:22 +0100

    Migrate to apollo-express-server

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,43 @@
 import dotenv from 'dotenv-safe'
-import { ApolloServer } from 'apollo-server'
+import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+import * as http from 'http'
 import { context } from './context'
 import { schema } from '@src/utils/generateSchema'
+import {
+  ApolloServerPluginDrainHttpServer,
+  ApolloServerPluginLandingPageGraphQLPlayground,
+  ApolloServerPluginLandingPageProductionDefault,
+} from 'apollo-server-core';
 
 dotenv.config()
 
-new ApolloServer({ schema, context: context }).listen({ port: 4000 }, () =>
-  console.log(`
-🚀 Server ready at: http://localhost:4000
-⭐️ See sample queries: http://pris.ly/e/ts/graphql-sdl-first#using-the-graphql-api`)
-)
+async function bootstrap() {
+
+  const app = express();
+  const httpServer = http.createServer(app)
+
+  const server = new ApolloServer({
+    schema,
+    context: context,
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      process.env.NODE_ENV === 'production'
+        ? ApolloServerPluginLandingPageProductionDefault()
+        : ApolloServerPluginLandingPageGraphQLPlayground(),
+    ],
+  });
+
+  await server.start();
+  server.applyMiddleware({ app });
+
+  app.listen({ port: 4000 }, () => {
+    console.log('App is listening on http://localhost:4000');
+  });
+
+}
+
+bootstrap()
+  .catch((err) => {
+    console.error(err)
+  });
