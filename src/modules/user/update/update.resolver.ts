3c56d9ae145aa47fd3869d48b8d2065addf7cdc2
dwozniak-guidance <dominik.wozniak@guidance.com>
2022-02-06 19:27:55 +0100

    Add update user resolver

---
+++ src/modules/user/update/update.resolver.ts
@@ -0,0 +1,57 @@
+import * as yup from 'yup'
+import { UserInputError } from 'apollo-server'
+import { Context } from '@src/context'
+import { formatYupError } from '@src/utils/format-yup-error'
+import { ResolverMap } from '@src/utils/graphql-types'
+import { applyMiddleware } from '@src/middleware/apply-middleware'
+import { authorization } from '@src/middleware/authorization.middleware'
+import { UpdateUserInput } from '@src/modules/user/update/update-user.input';
+
+const updateUser = yup.object().shape({
+  email: yup.string().email(),
+  name: yup.string(),
+})
+
+const resolvers: ResolverMap = {
+  Mutation: {
+    updateUser: applyMiddleware(
+      authorization,
+      async (
+        _parent,
+        args: { data: UpdateUserInput },
+        context: Context
+      ) => {
+
+        try {
+          await updateUser.validate(args.data, {
+            abortEarly: false,
+          })
+        } catch (error) {
+          throw new UserInputError(
+            'Cannot update profile with provided data',
+            formatYupError(error)
+          )
+        }
+
+        try {
+          await context.prisma.user.update({
+            where: {
+              // @ts-ignore
+              id: context.userId
+            },
+            data: {
+              ...args.data
+            }
+          })
+        } catch (error) {
+          console.error(error)
+          return false
+        }
+
+        return true
+      }
+    ),
+  },
+}
+
+export default resolvers
