cc3736b031d9a5c0f68ed3780010d7819a3f85d8
dwozniak-guidance <dominik.wozniak@guidance.com>
2022-02-16 10:19:10 +0100

    Rename message model

---
+++ prisma/migrations/20220215212610_rename/migration.sql
@@ -0,0 +1,33 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Message` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Message" DROP CONSTRAINT "Message_conversationId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Message" DROP CONSTRAINT "Message_userId_fkey";
+
+-- DropTable
+DROP TABLE "Message";
+
+-- CreateTable
+CREATE TABLE "ConversationMessage" (
+    "id" SERIAL NOT NULL,
+    "text" TEXT NOT NULL,
+    "read" BOOLEAN NOT NULL DEFAULT false,
+    "userId" INTEGER NOT NULL,
+    "conversationId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "ConversationMessage_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "ConversationMessage" ADD CONSTRAINT "ConversationMessage_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ConversationMessage" ADD CONSTRAINT "ConversationMessage_conversationId_fkey" FOREIGN KEY ("conversationId") REFERENCES "Conversation"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
