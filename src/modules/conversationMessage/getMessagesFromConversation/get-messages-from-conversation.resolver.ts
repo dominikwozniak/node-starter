9442ab8510f17ca5d4b028d3a813c059dd7f53e0
dwozniak-guidance <dominik.wozniak@guidance.com>
2022-02-16 17:45:03 +0100

    Fix get messages resolver type

--- src/modules/conversationMessage/getMessagesFromConversation/get-messages-from-conversation.resolver.ts
+++ src/modules/conversationMessage/getMessagesFromConversation/get-messages-from-conversation.resolver.ts
@@ -6,19 +6,21 @@ import { ResolverMap } from '@src/utils/graphql-types'
 import { authorization } from '@src/middleware/authorization.middleware'
 import { applyMiddleware } from '@src/middleware/apply-middleware'
 import { formatYupError } from '@src/utils/format-yup-error'
-import {
-  GetMessagesFromConversationInput
-} from '@src/modules/conversationMessage/getMessagesFromConversation/get-messages-from-conversation.input'
+import { GetMessagesFromConversationInput } from '@src/modules/conversationMessage/getMessagesFromConversation/get-messages-from-conversation.input'
 
 const getMessagesFromConversationSchema = yup.object().shape({
   conversationId: yup.number().min(0),
 })
 
 const resolvers: ResolverMap = {
-  Mutation: {
+  Query: {
     getMessagesFromConversation: applyMiddleware(
       authorization,
-      async (_parent, args: { data: GetMessagesFromConversationInput }, context: Context) => {
+      async (
+        _parent,
+        args: { data: GetMessagesFromConversationInput },
+        context: Context
+      ) => {
         const { conversationId } = args.data
 
         try {
@@ -36,11 +38,11 @@ const resolvers: ResolverMap = {
         // TODO: try-catch
         const messages = await context.prisma.conversationMessage.findMany({
           where: {
-            conversationId
+            conversationId,
           },
           include: {
-            author: true
-          }
+            author: true,
+          },
         })
 
         return messages
